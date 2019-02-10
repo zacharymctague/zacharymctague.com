4c4ac8bd608efe9daa4e22d6144c76f0b76b43a2
Zach <zach@blstr.co>
2019-02-10 18:26:02 +1100

    remove unused var

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { GlobalStyle, theme } from './_theme'
 
 // _app persists state and injects data into pages
 export default class extends App {
-  static async getInitialProps({ Component, router, ctx: context }: AppComponentContext) {
+  static async getInitialProps({ Component, ctx: context }: AppComponentContext) {
     let pageProps = {}
 
     if (Component.getInitialProps) {
